0513e47b610115d856e266ae279526739ef3cd10
LewisNicoll1 <52451661+LewisNicoll1@users.noreply.github.com>
2020-05-19 15:19:10 +0100

    Added GameManager
    Added basis of a game manager class and a slow motion manager class. Tidied some scripts up into folders.

---
+++ 3D Twin Stick Proto with Menu/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Create an instance of the game manager:
+    static GameManager instance;
+
+    //Get function to return an a reference of the game manager:
+    public static GameManager GetGameManager()
+    {
+        return instance;
+    }
+
+    SlowMoManager slowMoMangaer;
+
+
+    // Awake is called befoee anything else:
+    void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else if (instance == null)
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(this.gameObject);
+        }
+        
+    }
+
+    //Start is called before the first frame of update:
+    void Start()
+    {
+        slowMoMangaer = new SlowMoManager();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slowMoMangaer.SlowTime(0.05f);
+    }
+}
---
+++ 3D Twin Stick Proto with Menu/Assets/Scripts/SlowMoManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowMoManager
+{
+    private float defaultSlowTimeScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultSlowTimeScale = .05f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SlowTime()
+    {
+        Time.timeScale = defaultSlowTimeScale;
+    }
+
+    public void SlowTime(float timeScale)
+    {
+        Time.timeScale = timeScale;
+    }
+}
